20af23d60e16964e9698d87356b79c5444a25220
Joana Palero <joana.palero24@gmail.com>
2018-06-28 21:28:49 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,20 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    "Ola.".end_with?(".")
+    self.end_with?(".")
     
   end
 
   def question?
-  "Ola?".end_with?("?")
+    self.end_with?("?")
   end
 
   def exclamation?
-  "Ola means hello in Spanish.
-  Correct?" .c
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    
   end
 end
